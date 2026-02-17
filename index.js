d43439ba208090868c859a893e76abf95951f375
Gabriel Souza <gabrielsouzades@gmail.com>
2026-02-16 23:20:38 -0300

    refactor: routes groups

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import authRoutes from './src/routes/authRoutes.js';
+import routes from './src/routes/index.js';
 import errorHandler from './src/middlewares/errorHandler.js';
-import swaggerUi from 'swagger-ui-express';
-import openapiSpec from './src/docs/openapi.js';
 
 const app = express();
 app.use(cors());
@@ -12,10 +10,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // Rotas agrupadas
-app.use('/api/auth', authRoutes);
-
-// Swagger UI
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
+app.use(routes);
 
 // Middleware
 app.use(errorHandler);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import defaultRoutes from './defaultRoutes.js';
 import authRoutes from './authRoutes.js';
+import swaggerUi from 'swagger-ui-express';
+import openapiSpec from '../docs/openapi.js';
 
 const router = express.Router();
 
 router.use('/', defaultRoutes);
-router.use('/auth', authRoutes);
+router.use('/api/auth', authRoutes);
+router.use('/api/docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 export default router;
